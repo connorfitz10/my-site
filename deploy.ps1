cc01eddf6a84a42b06c97836d4dfad17776eb43e
Connor FitzGerald <connorfitz10@gmail.com>
2026-03-26 17:00:37 -0700

    fix PowerShell deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,5 @@
 param([string]$msg = "update site")
-quarto render && git add . && git commit -m $msg && git push
+quarto render
+if ($LASTEXITCODE -eq 0) { git add . }
+if ($LASTEXITCODE -eq 0) { git commit -m $msg }
+if ($LASTEXITCODE -eq 0) { git push }
